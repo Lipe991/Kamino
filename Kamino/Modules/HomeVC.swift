2190b1bd351a9df504f7e1d99bf12ad9a813dba4
Coin Sea <admin@coinsea.net>
2019-11-10 08:54:17 +0100

    Added initial HomeVC

---
+++ Kamino/Modules/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVC.swift
+//  Kamino
+//
+//  Created by Sandi Mihelic on 10/11/2019.
+//  Copyright © 2019 Sandi Mihelic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class HomeVC: UIViewController {
+    override func viewDidLoad() {
+        super.loadView()
+        
+        view.backgroundColor = .red
+    }
+}
